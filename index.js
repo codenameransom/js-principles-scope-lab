d912aa877b37b638bbbf09e041d3c7ede9c76f3f
codenameransom <codejoshuaroque@gmail.com>
2021-04-08 00:17:13 -0400

    scope is actually really fun git add .  @_@

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = "bob";
+const random = "changed the constant";
+
+function upperCaseCustomerName(){
+     customerName = "BOB";
+    return;
+}
+
+function setBestCustomer(){
+     bestCustomer = `not bob`;
+    return;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return;
+}
+
+function leastFavoriteCustomer(){
+    const random = "leastFavoriteCustomer";
+    return;
+}
+
+function changeLeastFavoriteCustomer(){
+      random = "Smegma"; //please dont not hire me ITS A JOKE!
+    return;
+}
